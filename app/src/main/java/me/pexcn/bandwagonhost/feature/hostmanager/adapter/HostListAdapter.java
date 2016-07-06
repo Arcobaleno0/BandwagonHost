33febc07c71ce9a93ab8218e650996b1d7cceaee
pexcn <pexcn97@gmail.com>
2016-07-06 11:00:52 +0800

    add popupmenu

--- app/src/main/java/me/pexcn/bandwagonhost/feature/hostmanager/adapter/HostListAdapter.java
+++ app/src/main/java/me/pexcn/bandwagonhost/feature/hostmanager/adapter/HostListAdapter.java
@@ -1,7 +1,10 @@
 package me.pexcn.bandwagonhost.feature.hostmanager.adapter;
 
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -10,6 +13,7 @@ import java.util.List;
 
 import me.pexcn.bandwagonhost.R;
 import me.pexcn.bandwagonhost.bean.Host;
+import me.pexcn.bandwagonhost.utils.common.LogUtils;
 
 /**
  * Created by pexcn on 2016-07-01.
@@ -37,12 +41,49 @@ public class HostListAdapter extends RecyclerView.Adapter<HostListAdapter.ViewHo
         return mHosts.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener,
+            PopupMenu.OnMenuItemClickListener {
         TextView mTextView;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            itemView.setOnLongClickListener(this);
             mTextView = (TextView) itemView.findViewById(R.id.tv_title);
+        }
+
+        private void showPopupMenu() {
+            PopupMenu popupMenu = new PopupMenu(itemView.getContext(), itemView, Gravity.END);
+            popupMenu.inflate(R.menu.menu_popupmenu);
+            popupMenu.setOnMenuItemClickListener(this);
+            popupMenu.show();
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            if (v == itemView) {
+                showPopupMenu();
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.menu_update:
+                    LogUtils.d("更新  -> " + mTextView.getText().toString());
+                    return true;
+                case R.id.menu_remove:
+                    LogUtils.d("删除 --> " + mTextView.getText().toString());
+                    return true;
+            }
+            return false;
         }
     }
 }
