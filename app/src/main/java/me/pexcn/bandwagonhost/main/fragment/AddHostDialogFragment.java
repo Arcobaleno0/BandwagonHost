ed9807a9bd9b161a330078fa052673191e030d82
pexcn <pexcn97@gmail.com>
2017-02-19 17:48:42 +0000

    Optimize handle input

--- app/src/main/java/me/pexcn/bandwagonhost/main/fragment/AddHostDialogFragment.java
+++ app/src/main/java/me/pexcn/bandwagonhost/main/fragment/AddHostDialogFragment.java
@@ -8,11 +8,14 @@ import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 import me.pexcn.bandwagonhost.R;
+import me.pexcn.bandwagonhost.bean.database.Host;
 
 /**
  * Created by pexcn on 2017-02-19.
@@ -51,21 +54,10 @@ public class AddHostDialogFragment extends DialogFragment
                 .setOnKeyListener(this)
                 .create();
         mDialog.setOnShowListener(dialog -> mDialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(v -> {
-            String string = getResources().getString(R.string.dialog_text_not_be_empty);
-            if (mTitle.length() == 0) {
-                mTitleLayout.setError(mTitleLayout.getHint() + " " + string);
+            if (mTitle.length() == 0 || mVeid.length() == 0 || mKey.length() == 0) {
+                handleAndFixInput();
             } else {
-                mTitleLayout.setErrorEnabled(false);
-            }
-            if (mVeid.length() == 0) {
-                mVeidLayout.setError(mVeidLayout.getHint() + " " + string);
-            } else {
-                mVeidLayout.setErrorEnabled(false);
-            }
-            if (mKey.length() == 0) {
-                mKeyLayout.setError(mKeyLayout.getHint() + " " + string);
-            } else {
-                mKeyLayout.setErrorEnabled(false);
+                handleAddHost();
             }
         }));
         return mDialog;
@@ -80,5 +72,77 @@ public class AddHostDialogFragment extends DialogFragment
             return true;
         }
         return false;
+    }
+
+    private void handleAndFixInput() {
+        mTitle.addTextChangedListener(new AddHostDialogTextWatcher(AddHostDialogTextWatcher.TYPE_TITLE));
+        mVeid.addTextChangedListener(new AddHostDialogTextWatcher(AddHostDialogTextWatcher.TYPE_VEID));
+        mKey.addTextChangedListener(new AddHostDialogTextWatcher(AddHostDialogTextWatcher.TYPE_KEY));
+
+        String string = getResources().getString(R.string.dialog_text_not_be_empty);
+        if (mTitle.length() == 0) {
+            mTitleLayout.setError(mTitleLayout.getHint() + " " + string);
+        } else {
+            mTitleLayout.setErrorEnabled(false);
+        }
+        if (mVeid.length() == 0) {
+            mVeidLayout.setError(mVeidLayout.getHint() + " " + string);
+        } else {
+            mVeidLayout.setErrorEnabled(false);
+        }
+        if (mKey.length() == 0) {
+            mKeyLayout.setError(mKeyLayout.getHint() + " " + string);
+        } else {
+            mKeyLayout.setErrorEnabled(false);
+        }
+    }
+
+    private void handleAddHost() {
+        final Host host = new Host();
+        host.title = mTitle.getText().toString();
+        host.veid = mVeid.getText().toString();
+        host.key = mKey.getText().toString();
+    }
+
+    private class AddHostDialogTextWatcher implements TextWatcher {
+        static final int TYPE_TITLE = 1;
+        static final int TYPE_VEID = 2;
+        static final int TYPE_KEY = 3;
+        private int mType;
+
+        AddHostDialogTextWatcher(int type) {
+            this.mType = type;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            switch (mType) {
+                case TYPE_TITLE:
+                    if (mTitleLayout.getError() != null) {
+                        mTitleLayout.setError(null);
+                    }
+                    break;
+                case TYPE_VEID:
+                    if (mVeidLayout.getError() != null) {
+                        mVeidLayout.setError(null);
+                    }
+                    break;
+                case TYPE_KEY:
+                    if (mKeyLayout.getError() != null) {
+                        mKeyLayout.setError(null);
+                    }
+                    break;
+            }
+        }
     }
 }
