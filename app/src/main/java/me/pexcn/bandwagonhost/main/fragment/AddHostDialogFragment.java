7fbcc60f9d9c6d5e1f8ffdb34b8d8514445a5577
pexcn <pexcn97@gmail.com>
2017-02-19 17:01:47 +0000

    AddHostDialogFragment

--- app/src/main/java/me/pexcn/bandwagonhost/main/fragment/AddHostDialogFragment.java
+++ app/src/main/java/me/pexcn/bandwagonhost/main/fragment/AddHostDialogFragment.java
@@ -4,23 +4,22 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 
 import me.pexcn.bandwagonhost.R;
-import me.pexcn.simpleutils.common.LogUtils;
 
 /**
- * Created by Administrator on 2017-02-19 0019.
+ * Created by pexcn on 2017-02-19.
  */
 public class AddHostDialogFragment extends DialogFragment
-        implements View.OnClickListener, DialogInterface.OnClickListener,
-        DialogInterface.OnKeyListener {
+        implements DialogInterface.OnKeyListener {
+    private AlertDialog mDialog;
     private TextInputEditText mTitle;
     private TextInputEditText mVeid;
     private TextInputEditText mKey;
@@ -35,27 +34,41 @@ public class AddHostDialogFragment extends DialogFragment
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog dialog = new AlertDialog.Builder(getActivity())
-                .setView(R.layout.dialog_add_host)
-                .setTitle(getResources().getString(R.string.activity_add_host_title))
-                .setNegativeButton(getResources().getString(android.R.string.cancel), null)
-                .setPositiveButton(getResources().getString(android.R.string.ok), this)
-                .setOnKeyListener(this)
-                .create();
         setCancelable(false);
-        return dialog;
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        final View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_add_host,
+                (ViewGroup) getActivity().getWindow().getDecorView(), false);
         mTitle = (TextInputEditText) view.findViewById(R.id.et_title);
         mVeid = (TextInputEditText) view.findViewById(R.id.et_veid);
         mKey = (TextInputEditText) view.findViewById(R.id.et_key);
         mTitleLayout = (TextInputLayout) view.findViewById(R.id.til_title);
         mVeidLayout = (TextInputLayout) view.findViewById(R.id.til_veid);
         mKeyLayout = (TextInputLayout) view.findViewById(R.id.til_key);
-        ((AlertDialog) getDialog()).getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(this);
+        mDialog = new AlertDialog.Builder(getActivity())
+                .setView(view)
+                .setTitle(getResources().getString(R.string.activity_add_host_title))
+                .setNegativeButton(getResources().getString(android.R.string.cancel), null)
+                .setPositiveButton(getResources().getString(android.R.string.ok), null)
+                .setOnKeyListener(this)
+                .create();
+        mDialog.setOnShowListener(dialog -> mDialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(v -> {
+            String string = getResources().getString(R.string.dialog_text_not_be_empty);
+            if (mTitle.length() == 0) {
+                mTitleLayout.setError(mTitleLayout.getHint() + " " + string);
+            } else {
+                mTitleLayout.setErrorEnabled(false);
+            }
+            if (mVeid.length() == 0) {
+                mVeidLayout.setError(mVeidLayout.getHint() + " " + string);
+            } else {
+                mVeidLayout.setErrorEnabled(false);
+            }
+            if (mKey.length() == 0) {
+                mKeyLayout.setError(mKeyLayout.getHint() + " " + string);
+            } else {
+                mKeyLayout.setErrorEnabled(false);
+            }
+        }));
+        return mDialog;
     }
 
     @Override
@@ -67,21 +80,5 @@ public class AddHostDialogFragment extends DialogFragment
             return true;
         }
         return false;
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case android.R.id.button1:
-
-                LogUtils.d(String.valueOf(mTitle.length()));
-
-                break;
-        }
-    }
-
-    @Override
-    public void onClick(DialogInterface dialog, int which) {
-
     }
 }
